c3595716add325c636952e40858cf162e768d934
Jonas Friedmann <j@frd.mn>
2015-12-05 13:24:24 +0100

    General: EOL cleanup

--- configs/zsh/appearance.zsh
+++ configs/zsh/appearance.zsh
@@ -12,7 +12,7 @@ if [ "$DISABLE_LS_COLORS" != "true" ]
 then
   # Find the option for using colors in ls, depending on the version: Linux or BSD
   if [[ "$(uname -s)" == "NetBSD" ]]; then
-    # On NetBSD, test if "gls" (GNU ls) is installed (this one supports colors); 
+    # On NetBSD, test if "gls" (GNU ls) is installed (this one supports colors);
     # otherwise, leave ls as is, because NetBSD's ls doesn't support -G
     gls --color -d . &>/dev/null 2>&1 && alias ls='gls --color=tty'
   elif [[ "$(uname -s)" == "OpenBSD" ]]; then
