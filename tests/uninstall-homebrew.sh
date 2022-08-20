3593840988c0914cf7c6747915cfa7285554217f
Jonas Friedmann <j@frd.mn>
2022-08-20 17:46:55 +0200

    Test: move uninstallation of Homebrew into CI file instead of separate script

--- tests/uninstall-homebrew.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -sLO https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh
-chmod +x ./uninstall.sh
-sudo ./uninstall.sh --force
-sudo rm -rf /usr/local/Homebrew /usr/local/Caskroom /usr/local/bin/brew
